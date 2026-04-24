1d0642db80e13446169ff488b9d1ae5cc47c176c
Violet <ianpk2001@gmail.com>
2026-04-24 08:03:13 -0500

    added utilization and timing reports

--- verilog/build.tcl
+++ verilog/build.tcl
@@ -4,6 +4,14 @@ read_xdc "./srcs/Arty-S7-25-Master.xdc"
 
 synth_design -top "top" -part "xc7s25csga324-1"
 
+report_utilization \
+    -file utilization.rpt
+
+report_timing_summary \
+    -file timing_summary.rpt \
+    -report_unconstrained \
+    -max_paths 10
+
 # place and route
 opt_design
 place_design
